1f5ccdf5dc725235b2ecae3237e88be32dcdcdb4
Katykaze <kathezambrano9@gmail.com>
2022-11-30 11:50:30 +0100

    getBuyInformation

--- Ejercicio2/php/functions.php
+++ Ejercicio2/php/functions.php
@@ -248,3 +248,24 @@ function getTotalProducts($conn, $product)
     //echo "<pre>";var_dump($resultado);echo "</pre>";
     return $resultado;
 }
+// Consulta de Compras (comconscom.php): se mostrarán en un desplegable los NIF de los
+// clientes, una fecha desde y una fecha hasta. Se mostrará por pantalla la información de las
+// compras realizadas por los clientes en ese periodo (producto, nombre producto, precio compra)
+// así como el montante total de todas las compras.
+function getDnies($conn){
+    try {
+        $sql = $conn->prepare("SELECT NIF FROM CLIENTE");
+        $sql->execute();
+        $sql->setFetchMode(PDO::FETCH_ASSOC);
+        $resultado = $sql->fetchAll();
+
+        return $resultado;
+    } catch (PDOException $e) {
+        return [];
+    }
+
+    return $resultado;
+}
+function getBuyInformation(){
+    
+}
